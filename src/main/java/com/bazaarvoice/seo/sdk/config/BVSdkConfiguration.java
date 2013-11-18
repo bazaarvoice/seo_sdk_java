6399b294c6c0c806285e113c769039569f54fc64
svetlana-garcia <svetlana.garcia@hp.com>
2013-11-18 15:26:17 -0600

    Update BVSdkConfiguration.java

--- src/main/java/com/bazaarvoice/seo/sdk/config/BVSdkConfiguration.java
+++ src/main/java/com/bazaarvoice/seo/sdk/config/BVSdkConfiguration.java
@@ -11,54 +11,52 @@ import com.bazaarvoice.seo.sdk.exception.BVSdkException;
 import com.bazaarvoice.seo.sdk.util.BVConstant;
 
 /**
- * Default implementation of configuration settings.
- * This loads the Bazaarvoice specific configuration
- * and also user specific setting.
- * 
+ * Default implementation of configuration settings. This loads the Bazaarvoice
+ * specific configuration and also user specific setting.
+ *
  * @author Anandan Narayanaswamy
  *
  */
 public class BVSdkConfiguration implements BVConfiguration {
-	
-	private static Logger _logger = LoggerFactory.getLogger(BVSdkConfiguration.class);
-	
-	private Map<String, String> _instanceConfiguration;
-	
 
-	/**
-	 * Default constructor.
-	 * If configuration should be overwritten or if you need to 
-	 * load properties/configuration every time please use the add method.
-	 */
-	public BVSdkConfiguration() {
-		_instanceConfiguration = new HashMap<String, String>();
-		
-		/*
-		 * Adding bvcore properties.
-		 */
-		_instanceConfiguration.put(BVCoreConfig.PRODUCTION_S3_HOSTNAME.getPropertyName(), BVConstant.PRODUCTION_S3_HOSTNAME);
-		_instanceConfiguration.put(BVCoreConfig.STAGING_S3_HOSTNAME.getPropertyName(), BVConstant.STAGING_S3_HOSTNAME);
-		
-		addProperty(BVClientConfig.EXECUTION_TIMEOUT, BVConstant.EXECUTION_TIMEOUT);
-		addProperty(BVClientConfig.CRAWLER_AGENT_PATTERN, BVConstant.CRAWLER_AGENT_PATTERN);
-		addProperty(BVClientConfig.CONNECT_TIMEOUT, BVConstant.CONNECT_TIMEOUT);
-		addProperty(BVClientConfig.SOCKET_TIMEOUT, BVConstant.SOCKET_TIMEOUT);
-		addProperty(BVClientConfig.STAGING, BVConstant.STAGING);
-		addProperty(BVClientConfig.SEO_SDK_ENABLED, BVConstant.SEO_SDK_ENABLED);
-		_logger.debug("Completed default properties in BVSdkConfiguration.");
-	}
-	
-	public BVConfiguration addProperty(BVClientConfig bvConfig, String propertyValue) {
-		if (StringUtils.isBlank(propertyValue)) {
-			throw new BVSdkException("ERR0006");
-		}
-		
-		this._instanceConfiguration.put(bvConfig.getPropertyName(), propertyValue);
-		return this;
-	}
-	
-	public String getProperty(String propertyName) {
-		return this._instanceConfiguration.get(propertyName);
-	}
-	
+    private static Logger _logger = LoggerFactory.getLogger(BVSdkConfiguration.class);
+    private Map<String, String> _instanceConfiguration;
+
+    /**
+     * Default constructor. If configuration should be overwritten or if you
+     * need to load properties/configuration every time please use the add
+     * method.
+     */
+    public BVSdkConfiguration() {
+        _instanceConfiguration = new HashMap<String, String>();
+
+        /*
+         * Adding bvcore properties.
+         */
+        _instanceConfiguration.put(BVCoreConfig.PRODUCTION_S3_HOSTNAME.getPropertyName(), BVConstant.PRODUCTION_S3_HOSTNAME);
+        _instanceConfiguration.put(BVCoreConfig.STAGING_S3_HOSTNAME.getPropertyName(), BVConstant.STAGING_S3_HOSTNAME);
+
+        addProperty(BVClientConfig.EXECUTION_TIMEOUT, BVConstant.EXECUTION_TIMEOUT);
+        addProperty(BVClientConfig.CRAWLER_AGENT_PATTERN, BVConstant.CRAWLER_AGENT_PATTERN);
+        addProperty(BVClientConfig.CONNECT_TIMEOUT, BVConstant.CONNECT_TIMEOUT);
+        addProperty(BVClientConfig.SOCKET_TIMEOUT, BVConstant.SOCKET_TIMEOUT);
+        addProperty(BVClientConfig.STAGING, BVConstant.STAGING);
+        addProperty(BVClientConfig.SEO_SDK_ENABLED, BVConstant.SEO_SDK_ENABLED);
+        addProperty(BVClientConfig.PROXY_HOST, BVConstant.PROXY_HOST);
+        addProperty(BVClientConfig.PROXY_PORT, BVConstant.PROXY_PORT);
+        _logger.debug("Completed default properties in BVSdkConfiguration.");
+    }
+
+    public BVConfiguration addProperty(BVClientConfig bvConfig, String propertyValue) {
+        if (StringUtils.isBlank(propertyValue)) {
+            throw new BVSdkException("ERR0006");
+        }
+
+        this._instanceConfiguration.put(bvConfig.getPropertyName(), propertyValue);
+        return this;
+    }
+
+    public String getProperty(String propertyName) {
+        return this._instanceConfiguration.get(propertyName);
+    }
 }
