63d7878371ed432ae0b8f73938f3911b38c90cbd
anandan-bazaarvoice <anandan.narayanaswamy@valtech.com>
2014-05-30 13:14:56 -0500

    Updated Documents and added apache license comments.

--- src/test/java/com/bazaarvoice/seo/sdk/BVManagedUIContent_ConnectionTimedout.java
+++ src/test/java/com/bazaarvoice/seo/sdk/BVManagedUIContent_ConnectionTimedout.java
@@ -1,3 +1,22 @@
+/*
+ * ===========================================================================
+ * Copyright 2014 Bazaarvoice, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===========================================================================
+ * 
+ */
+
 package com.bazaarvoice.seo.sdk;
 
 import java.util.HashMap;
@@ -9,7 +28,6 @@ import org.testng.annotations.Test;
 import com.bazaarvoice.seo.sdk.config.BVClientConfig;
 import com.bazaarvoice.seo.sdk.config.BVConfiguration;
 import com.bazaarvoice.seo.sdk.config.BVCoreConfig;
-import com.bazaarvoice.seo.sdk.config.BVSdkConfiguration;
 import com.bazaarvoice.seo.sdk.model.BVParameters;
 import com.bazaarvoice.seo.sdk.model.ContentType;
 import com.bazaarvoice.seo.sdk.model.SubjectType;
