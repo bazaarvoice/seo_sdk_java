2027e02fa904ab34e94b910569d690281df3e405
Jared Enriquez <jared.enriquez@bazaarvoice.com>
2016-02-09 22:04:09 -0600

    SEO-1169: Pom changes and modified unit tests
    [close #38]

--- src/test/java/com/bazaarvoice/seo/sdk/footer/BVHTMLFooterTest.java
+++ src/test/java/com/bazaarvoice/seo/sdk/footer/BVHTMLFooterTest.java
@@ -56,7 +56,7 @@ public class BVHTMLFooterTest {
 
     StringBuilder expectedFooterPattern = new StringBuilder();
     expectedFooterPattern.append("\\Q<ul id=\"BVSEOSDK_meta\" style=\"display:none !important\">\\E")
-    .append("\\s\\s\\Q<li data-bvseo=\"sdk\">bvseo_sdk, java_sdk, bvseo-\\E\\d+\\.\\d+\\.\\d+\\Q</li>\\E")
+    .append("\\s\\s\\Q<li data-bvseo=\"sdk\">bvseo_sdk, java_sdk, bvseo-\\E\\d+\\.\\d+\\.\\d+(\\-SNAPSHOT)?\\Q</li>\\E")
     .append("\\s\\s\\Q<li data-bvseo=\"sp_mt\">CLOUD, getContent, 0ms</li>\\E")
     .append("\\s\\s\\Q<li data-bvseo=\"ct_st\">REVIEWS, PRODUCT</li>\\E")
     .append("\\Q</ul>\\E");
@@ -313,7 +313,7 @@ public class BVHTMLFooterTest {
   private String getDbgFtrPtrn(String pageURI) {
     StringBuilder sBuilder = new StringBuilder();
     sBuilder.append("\\Q<ul id=\"BVSEOSDK_meta\" style=\"display:none !important\">\\E")
-      .append("\\s\\s\\Q<li data-bvseo=\"sdk\">bvseo_sdk, java_sdk, bvseo-\\E\\d+\\.\\d+\\.\\d+\\Q</li>\\E")
+      .append("\\s\\s\\Q<li data-bvseo=\"sdk\">bvseo_sdk, java_sdk, bvseo-\\E\\d+\\.\\d+\\.\\d+(\\-SNAPSHOT)?\\Q</li>\\E")
       .append("\\s\\s\\Q<li data-bvseo=\"sp_mt\">LOCAL, getContent, 0ms</li>\\E")
       .append("\\s\\s\\Q<li data-bvseo=\"ct_st\">REVIEWS, PRODUCT</li>\\E")
       .append("\\Q</ul>\\E")
